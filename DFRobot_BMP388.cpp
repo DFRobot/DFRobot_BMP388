7075b03a5bebcc1486cb3642a6881aa95912fa5e
mengbishu <841616394@qq.com>
2018-06-05 17:36:55 +0800

    v0.1

--- DFRobot_BMP388.cpp
+++ DFRobot_BMP388.cpp
@@ -1,3 +1,17 @@
+/*!
+ * @file DFRobot_BMP388.cpp
+ * @brief DFRobot's DFRobot_BMP388
+ * @n DFRobot's Temperature, Pressure and Approx altitude
+ *    Supports the SPI/I2C communication with Arduino.
+ *
+ * @copyright	[DFRobot](http://www.dfrobot.com), 2016
+ * @copyright	GNU Lesser General Public License
+ *
+ * @author [yuhao](yuhao.lu@dfrobot.com)
+ * @version  V1.0
+ * @date  2018-5-29
+ */
+
 #include "DFRobot_BMP388.h"
 #include "bmp3_defs.h"
 #include "Arduino.h"
